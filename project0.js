47b98ef9ec57100bdcfc9827c72465cc075388cf
Danielle <danisteele1984@gmail.com>
2021-01-06 14:49:31 +1100

    added render function

--- project0.js
+++ project0.js
@@ -1,12 +1,9 @@
 const player1 = "Player1";
 const player2 = "Player2";
 let turn = player1;
-
-const player = document.getElementById("player")
-// console.log(player)
-const cells = document.getElementsByClassName("cell") 
-// console.log(cells.a1)
-
+let winner;
+const player = document.getElementById( "player" )
+const cells = document.getElementsByClassName( "cell" )
 const state = ["", "", "", "", "", "",  "", "", ""]
 const win = [
     [0, 1, 2],
@@ -20,75 +17,78 @@ const win = [
 ]
 let gameOver = false;
 
-const won = () => {
-    for (let i = 0; i < win.length; i++) {
+const checkingForWinner = () => {
+    for ( let i = 0; i < win.length; i++ ) {
         const winCondition = win[i];
-        const condition0 = state[winCondition[0]]
-        const condition1 = state[winCondition[1]]
-        const condition2 = state[winCondition[2]]
-        console.log("------");
-        console.log("index:", i, "winCondition", winCondition);
-        console.log("condition0", condition0, "condition1", condition1, "condition2", condition2);
-        // console.log(state, winCondition);
-        if(condition0 === "X" && condition1 === "X" && condition2 === "X") {
+        const condition0 = state[ winCondition[0] ]
+        const condition1 = state[ winCondition[1] ]
+        const condition2 = state[ winCondition[2] ]
+        if( condition0 === "X" && condition1 === "X" && condition2 === "X" ) {
             gameOver = true;
-            console.log("Player2 wins");
+            winner = player1;
         } 
-        if(condition0 === "O" && condition1 === "O" && condition2 === "O") {
+        if( condition0 === "O" && condition1 === "O" && condition2 === "O" ) {
             gameOver = true;
-            console.log("Player1 wins");
+            winner = player2;
         }
     }
 }
-const draw = () => {
-    if (gameOver === true) {
+const checkingForDraw = () => {
+    if ( gameOver === true ) {
         return;
     }
     let movesRemaining = false;
-    for(let i = 0; i < state.length; i++) {
-        if (state[i] === "") {
+    for( let i = 0; i < state.length; i++ ) {
+        if ( state[i] === "" ) {
             movesRemaining = true;
         }
     }
-    if (movesRemaining === false) {
+    if ( movesRemaining === false ) {
         gameOver = true;
-        console.log("draw");
     }
 }
 
 const cellClicked = (event) => {
     const cellId = event.target.id
-    const index = event.target.getAttribute("data-index")
-    if(gameOver===true) {
+    const index = event.target.getAttribute( "data-index" )
+    if( gameOver === true ) {
         return;
     }
     if(state[index] !== "") {
         return;
     } 
-    if (turn === player1) {
+    if ( turn === player1 ) {
         turn = player2;
-        player.innerHTML ="turn: player2";
         state[index] ="X";
-        document.getElementById(cellId).setAttribute("class","cell X");
+        document.getElementById( cellId ).setAttribute( "class","cell X" );
     } else {
         turn = player1;
-        player.innerHTML ="turn: player1";
         state[index] ="O";
         document.getElementById(cellId).setAttribute("class","cell O");
     }
-    console.log(state)
-    won()
-    draw()
+    checkingForWinner();
+    CheckingForDraw();
+    render();
 }
 
-for (let i=0; i < cells.length; i++){
-    cells[i].addEventListener("click", cellClicked)
+for ( let i=0; i < cells.length; i++ ) {
+    cells[i].addEventListener( "click", cellClicked )
 }
 
+const render = () => {
+    if ( turn === player1 ) {
+        player.innerHTML = "Turn: Player1";
+    } else {
+        player.innerHTML = "Turn: Player2";
+    }
+    if ( winner === player1 ) {
+        player.innerHTML = "Player1 Wins";
+    } else if ( winner === player2 ) {
+        player.innerHTML = "Player2 Wins";
+    } else if ( gameOver === true ) {
+        player.innerHTML = "DRAW.. Rematch? >:-)";
+    }
+}
 
-// Any of these combinations is a win: array of arrays.
-// store and check your rules. 
-// 0, 1, 2 break it up smaller
-
+// break it up smaller
 // Check:
-// Valid move - is box empty or taken
